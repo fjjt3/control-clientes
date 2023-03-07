cbcb244a8fba47fec9c3ffebbd73b521f4f5cd97
fjjt3 <fjjt3@yahoo.com>
2023-03-07 11:03:41 +0100

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firestore: {apiKey: "AIzaSyCbZQHb2yV-fByfnNa_FWDqiREgJjGON18",
+    authDomain: "control-clientes-78ce4.firebaseapp.com",
+    projectId: "control-clientes-78ce4",
+    storageBucket: "control-clientes-78ce4.appspot.com",
+    messagingSenderId: "333162849599",
+    appId: "1:333162849599:web:989240de5248ec8183df8a",
+    measurementId: "G-NWTSY9D1W1"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firestore: {apiKey: "AIzaSyCbZQHb2yV-fByfnNa_FWDqiREgJjGON18",
+    authDomain: "control-clientes-78ce4.firebaseapp.com",
+    projectId: "control-clientes-78ce4",
+    storageBucket: "control-clientes-78ce4.appspot.com",
+    messagingSenderId: "333162849599",
+    appId: "1:333162849599:web:989240de5248ec8183df8a",
+    measurementId: "G-NWTSY9D1W1"
+  }
 };
 
 /*
